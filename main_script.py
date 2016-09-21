f73468cc8786a47224cc0a7f9d259b9306efecf4
Vijay Vammi <mesanthu@gmail.com>
2016-09-21 15:52:26 +0100

    Kickstarterdata dumped into Mongo

--- main_script.py
+++ main_script.py
@@ -1,6 +1,8 @@
 import scrape_crowdcube as sc
 
 from pymongo import MongoClient
+import urllib2
+import json
 
 client = MongoClient()
 
@@ -28,4 +30,25 @@ def analyze_investements_mongo():
     initial = { 'count':0, 'sum':0}
     reduce = 'function(doc, out) {out.count++, out.sum+=doc.amount_raised}'
     result = db.live_investements.group(key, condition, initial, reduce)[0] 
-    return result['count'], result['sum']         
+    return result['count'], result['sum']
+
+def dump_kickstarter():
+    base_url = 'https://www.kickstarter.com/discover/advanced?google_chrome_workaround&category_id=1&woe_id=0&sort=magic&seed=2457444'
+    hdr = {'X-Requested-With': 'XMLHttpRequest',
+            'Accept': 'application/json, text/javascript, */*; q=0.01'}
+    #since the api only gives 20 at a time.
+    total_rows = 0
+    for i in range(5):
+        url = base_url + '&page=' + str(i+1)
+        req = urllib2.Request(url, headers = hdr)
+        response = urllib2.urlopen(req)
+        if response.getcode() != 200:
+            print ' Houston, we have a problem!!'
+            return 0
+        json_content = json.loads(response.read())
+        results = db.kickstarter.insert_many([x for x in json_content['projects']])
+        total_rows += len(results.inserted_ids)
+        print 'Inserted ' + str(len(results.inserted_ids)) + ' rows. Total inserted till now ' + str(total_rows) 
+        if json_content['has_more'] != True:
+            break    
+    print 'Total rows in kickstarter collection: ' + str(db.kickstarter.count())        
